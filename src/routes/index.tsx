188acda4f4683b370982eb9df4458412a4b9bd62
Sebastian Cardoso <hi@sebastiancardoso.com>
2025-07-18 04:26:50 -0300

    feat: add Button component with variants and integrate into main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui";
 
 export default component$(() => {
   return (
@@ -9,6 +10,7 @@ export default component$(() => {
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
+        <Button>PRUEBA</Button>
       </div>
     </>
   );
