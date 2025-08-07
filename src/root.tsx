4209026090dc1b0148dad471818bb1782f759f4d
Sebastian Cardoso <hi@sebastiancardoso.com>
2025-08-07 20:47:49 -0300

    feat: integrate Partytown for improved performance and add Google Analytics 4 support with configuration in root component

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,11 @@
 import { component$, isDev } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
+import { QwikPartytown } from './components/partytown/partytown';
 
 import "./global.css";
+
+const GA_MEASUREMENT_ID = 'G-JYLV331JV6';
 
 export default component$(() => {
   /**
@@ -23,6 +26,28 @@ export default component$(() => {
           />
         )}
         <RouterHead />
+
+        {/* Configuración de Partytown */}
+        <QwikPartytown forward={['gtag', 'dataLayer.push']} />
+
+        {/* Google Analytics 4 con Partytown */}
+        <script
+          async
+          type="text/partytown"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_title: document.title,
+              page_location: window.location.href
+            });
+          `}
+        />
       </head>
       <body lang="en">
         <RouterOutlet />
